0892b61dd89b9add6d2acef13fb0120f137409c6
CarlosFelipeF <25907953+CarlosFelipeF@users.noreply.github.com>
2025-02-18 01:48:59 +1000

    Update Wiz_AWS.tf

--- infra/Wiz_AWS.tf
+++ infra/Wiz_AWS.tf
@@ -229,7 +229,7 @@ resource "aws_instance" "mongo" {
     apt-get update -y
     apt-get install -y gnupg wget
     wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
-    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
     apt-get update -y && apt-get install -y mongodb-org
     systemctl start mongod
     systemctl enable mongod
